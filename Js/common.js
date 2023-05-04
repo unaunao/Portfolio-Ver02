c0c2b8871f1ee527583c4806ec2c915318c80515
una <cheerioespana@gmail.com>
2023-05-04 16:41:54 +0900

    body 슬라이드 기본 형태 맞춤

--- Js/common.js
+++ Js/common.js
@@ -1,7 +1,10 @@
 $("document").ready(function () {
+
+  /* 메인 body 화면 슬라이드 */
   var swiper = new Swiper('.swiper-pages', {
     autoHeight: true, //enable auto height
     spaceBetween: 0,
+    loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -11,4 +14,28 @@ $("document").ready(function () {
       prevEl: '.swiper-button-prev',
     },
   });
+
+   /* 다음 슬라이드로 넘어갈때 항상위로 */
+  function activeHeightSet() {
+    var activeHt = $(".swiper-slide-active .mainImg").height();
+    eventActiveHt(activeHt);
+    scrollUp();
+  }
+
+  function eventActiveHt(activeHt) {
+    $(".swiper-container").stop(true).animate(
+      {
+      },
+      100
+    );
+  }
+  function scrollUp() {
+    $("body, html").stop(true).animate(
+      {
+      scrollTop: "0",
+      },
+      1
+    );
+  }
+  swiper.on("slideChangeTransitionStart", activeHeightSet);
 })
