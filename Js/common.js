3c9c23a67fbe677c43dfc920cf38d935bfb5a7c1
una <cheerioespana@gmail.com>
2023-05-08 18:18:33 +0900

    header, font 스타일 정리 중

--- Js/common.js
+++ Js/common.js
@@ -1,3 +1,22 @@
+// JavaScript
+// Navigation bar scrollEvent
+const nav_bar = document.querySelector(".nav");
+
+function scrollEvent () {
+  document.addEventListener ("scroll", function(){
+    if(document.documentElement.scrollTop > 0) {
+      nav_bar.classList.add("nav-down");
+    } else {
+      nav_bar.classList.remove("nav-down");
+    }
+  });
+}
+function init() {
+  document.addEventListener("scroll", scrollEvent);
+}
+init();
+
+// jQuery
 $("document").ready(function () {
 
   /* 메인 body 화면 슬라이드 */
@@ -16,26 +35,21 @@ $("document").ready(function () {
   });
 
    /* 다음 슬라이드로 넘어갈때 항상위로 */
+  //  높이를 측정하여 스크롤 업 하는 함수 
   function activeHeightSet() {
-    var activeHt = $(".swiper-slide-active .mainImg").height();
-    eventActiveHt(activeHt);
+    // 스크롤업 함수 만든것을 불러옴
     scrollUp();
   }
-
-  function eventActiveHt(activeHt) {
-    $(".swiper-container").stop(true).animate(
-      {
-      },
-      100
-    );
-  }
+  // 스크롤 업 함수 
   function scrollUp() {
+    // 애니메이션으로 0.001초 만에 body와 html의 스크롤 위치 top 0로 이동
     $("body, html").stop(true).animate(
       {
-      scrollTop: "0",
+        scrollTop: "0",
       },
       1
     );
   }
   swiper.on("slideChangeTransitionStart", activeHeightSet);
+
 })
