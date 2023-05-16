ce0e3dfd213c9981ae1f3f13ec3322ea69e213fd
una <cheerioespana@gmail.com>
2023-05-16 20:26:23 +0900

    mobile navigaiton 세팅

--- js/common.js
+++ js/common.js
@@ -1,9 +1,8 @@
 /* 실행 */
-// Navigation bar scrollEvent
 $('document').ready(function () {
-  console.log('document ready complete');
+  // console.log('document ready complete');
 
-  // Sub페이지 불러오기
+  /* Pages 슬라이드 구조안에 불러오기 */
   function includeHTML(){
     let z, elmnt, file, xhttp;
     z = document.getElementsByTagName("*");
@@ -29,20 +28,6 @@ $('document').ready(function () {
       }//if - file
     }//for
 
-  const nav_bar = document.querySelector(".nav");
-
-  function scrollEvent () {
-    document.addEventListener ("scroll", function(){
-      if(document.documentElement.scrollTop > 0) {
-        nav_bar.classList.add("nav-down");
-      } else {
-        nav_bar.classList.remove("nav-down");
-      }
-    });
-  }
-
-  scrollEvent();
-
   /* 메인 body 화면 슬라이드 */
   var swiper = new Swiper('.swiper-pages', {
     autoHeight: true, //enable auto height
@@ -50,6 +35,7 @@ $('document').ready(function () {
     loop: true,
     pagination: {
       el: '.swiper-pagination',
+      // el: '.menu_list',
       clickable: true,
     },
     navigation: {
@@ -58,7 +44,45 @@ $('document').ready(function () {
       // prevEl: '.swiper-button-prev',
       prevEl: '.prev_btn'
     },
-  });
+  });//Swiper(swiper-pages)
+
+
+  var swiper = new Swiper('.swiper-pages', {
+    pagination: {
+      // el: '.swiper-pagination',
+      el: '.menu_list',
+      clickable: true,
+    },
+  });//Swiper(swiper-pages)
+
+
+  /* page navigation에 tage 추가 */
+  function addTag() {
+    const modeTag = document.getElementsByClassName('swiper-pagination')[0];
+    const htmlBtn = document.createElement('button');
+    modeTag.appendChild(htmlBtn);
+  }
+
+  addTag();
+
+  /* Navigation class on 넣어 열고 닫기*/
+  let navBtn = document.getElementsByClassName('btn-m-gnb')[0];
+  let mobileNav = document.getElementsByClassName('mobile-nav-overlay')[0];
+  let closeBtn = document.getElementsByClassName('close')[0];
+  navBtn.onclick = function navOpen() {
+    mobileNav.classList.add("on");
+  };
+  closeBtn.onclick = function navOpen() {
+    console.log(mobileNav);
+    mobileNav.classList.remove("on");
+  };
+  window.addEventListener('resize', function(){
+    let bodyWidth = document.body.offsetWidth;
+    console.log(bodyWidth);
+    if (bodyWidth >= 1183){
+      mobileNav.classList.remove("on");
+    };
+  })
 
    /* 다음 슬라이드로 넘어갈때 항상위로 */
   //  높이를 측정하여 스크롤 업 하는 함수 
@@ -82,5 +106,22 @@ $('document').ready(function () {
 
   }//includeHTML
 
+  /* Navigation bar scrollEvent 네비게이션 바 색상 넣기 */
+  const nav_bar = document.querySelector(".nav");
+
+  function scrollEvent () {
+    document.addEventListener ("scroll", function(){
+      if(document.documentElement.scrollTop > 0) {
+        nav_bar.classList.add("nav-down");
+      } else {
+        nav_bar.classList.remove("nav-down");
+      }
+    });
+    
+  }//scrollEvent
+  
+  
   includeHTML();
+  scrollEvent();
 });
+
