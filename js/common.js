8b3c54803b6c13b4bb2338980830a46fd174c50c
una <cheerioespana@gmail.com>
2023-05-26 15:42:42 +0900

    모바일 안쪽슬라이드 스크롤 관련 수정

--- js/common.js
+++ js/common.js
@@ -179,6 +179,7 @@ $('document').ready(function () {
     autoHeight: true,
     spaceBetween: 0,
     mousewheel: true,
+    touchRatio: 0,
     pagination: {
       el: '.work-pagination',
       clickable: true,
